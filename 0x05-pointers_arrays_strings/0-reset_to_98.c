a0e5aab42f4021cfb5bf5dcd3685495213edcb5b
ClareseD <claresedaniels67@gmail.com>
2022-09-19 13:12:18 -0700

    Task 0 98 battery str

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes a pointer to an int parameterans updates the value it points to 98
+ *
+ * @n: int parameter
+ *
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+*n - 98;
+}
