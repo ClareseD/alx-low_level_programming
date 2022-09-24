32fea872e10c2ed6d56d44d66c496fc6bf37a46c
ClareseD <claresedaniels67@gmail.com>
2022-09-24 11:51:25 -0700

    Automated push

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,23 +1,31 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _strcat - concatenates the string pointed to by @src to
- * the end the string pointed to by @dest
- * @dest: string that will be appended
- * @src: string to be concatenated upon
- *
- * Return: returns pointer to @dest
+ * *_strcat - function commute srtings
+ * @dest: param pointer to a char
+ * @src: param pointer to a char
+ * Return: return value of dest
  */
+
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int i;
+	int j;
 
-	while (dest[index++])
-		dest_len++;
+	i = 0;
+	j = 0;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
 
+	dest[i] = '\0';
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,1 +1,1 @@
-0. strcat
+Hello pointers
